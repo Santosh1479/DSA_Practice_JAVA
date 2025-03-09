33759c0b66680c0aaa1b77461f206bdfbbf452af
Santosh S <san.sational.s.b@gmail.com>
2025-03-09 18:41:40 +0530

    done

--- Queues.java
+++ Queues.java
@@ -54,18 +54,18 @@ public class Queues {
 
         }
 
-        public static void main(String[] args) {
-            Queue q = new Queue(5);
-            q.add(1);
-            q.add(2);
-            q.add(3);
+    }
 
-            while (!q.isEmpty()) {
-                System.out.println(q.peek());
-                q.remove();
-            }
+    public static void main(String[] args) {
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
         }
-
     }
 
 }
