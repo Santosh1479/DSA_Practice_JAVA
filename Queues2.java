9777c9829323add06baa2fe05e0f9aee26ca5485
Santosh S <san.sational.s.b@gmail.com>
2025-03-13 22:09:44 +0530

    ..

---
+++ Queues2.java
@@ -0,0 +1,89 @@
+public class Queues2 {
+
+    // using circular aarray.......................
+    static class Queue {
+        static int arr[];
+        static int size;
+        static int rear = -1;
+        static int front = -1;
+
+        Queue(int n) {
+            arr = new int[n];
+            this.size = n;
+        }
+
+        public static boolean isEmpty() {
+            return rear == -1 && front == -1;
+        }
+
+        public static boolean isFull() {
+            return (rear + 1) % size == front;
+        }
+        // enqueue or ADD
+
+        public void add(int data) {
+            if (isFull()) {
+                System.out.println("queue full");
+                return;
+            }
+            // at first element addition
+
+            if (front == -1) {
+                front = 0;
+            }
+            rear = (rear + 1) % size;
+            arr[rear] = data;
+
+        }
+
+        // DEQUEUE delete --0(1)
+
+        public int remove() {
+            if (isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+
+            int result = arr[front];
+            if (rear == front) {
+                rear = front = -1;
+            } else {
+                front = (front + 1) % size;
+            }
+            return result;
+        }
+
+        // PEEK --0(1)
+        public int peek() {
+            if (isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            return arr[front];
+
+        }
+    }
+
+    public static void main(String[] args) {
+            Queue q = new Queue(5);
+            q.add(1);
+            q.add(2);
+            q.add(3);
+            q.add(4);
+            q.add(5);
+            System.out.println(q.remove());
+            q.add(6);
+            System.out.println(q.remove());
+            q.add(7);
+
+
+            while (!q.isEmpty()) {
+                System.out.println(q.peek());
+                q.remove();
+            }
+    }
+}
+
+    
+
+
